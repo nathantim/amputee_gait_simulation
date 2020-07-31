c9f708405bd63744643cb4e193fb80ea64cd9305
nathantim <timmerhout@gmail.com>
2020-07-31 17:08:55 +0200

    Make getBodyMass, plotProgress, updateProgress general

--- plotProgressOptimization.m
+++ plotProgressOptimization.m
@@ -10,13 +10,11 @@ set(0, 'DefaultStairHitTest','off','DefaultStairPickableParts','none');
 set(0, 'DefaultLegendHitTest','off','DefaultLegendPickableParts','none');
 
 %%
-
 try
     dataFieldnames = fieldnames(data);
     %     numOfData = length(dataFieldnames)-1;
-    
-    if ~isempty(dataFieldnames) && length(dataFieldnames)>4
-        %%
+    %%
+    if ~isempty(dataFieldnames) && length(dataFieldnames)>4 && data.timeCost.data == 0 
         % updateFigure = findobj('type','figure','Name','Optimization Parameters');
         gaitKinematics = findobj('type','figure','Name','Gait Kinematics');
         musclesStimulation = findobj('type','figure','Name','Muscle stimulation levels');
@@ -43,56 +41,60 @@ try
             %     GRFData.HitTest = 'off';
             GRFData.Name = 'Ground reaction forces';
         end
+        %%
         if isempty(minCost) || data.cost.data < minCost
             minCost = data.cost.data;
             b_minCost = true;
         else
             b_minCost = false;
         end
-        
         %%
+        plotInfo.showSD = true;
         plotInfo.plotProp = {'LineStyle','Color','LineWidth'};
         plotInfo.lineVec = {'-'; '--';':'};
         plotInfo.colorProp = {	'#0072BD';	'#D95319';'#7E2F8E'};
-        plotInfo.fillProp = {'FaceColor','FaceAlpha','EdgeColor'};
-        plotInfo.fillVal = {[0.8 0.8 0.8],0.9,'none'};
         plotInfo.lineVec = plotInfo.lineVec(1:3,:);
         plotInfo.colorProp = plotInfo.colorProp(1:3,:);
         plotInfo.lineWidthProp = {3;3;3};
         plotInfo.plotProp_entries = [plotInfo.lineVec(:),plotInfo.colorProp(:), plotInfo.lineWidthProp(:)];
         
+        plotInfo.fillProp = {'FaceColor','FaceAlpha','EdgeColor','LineStyle'};
+        faceAlpha = {0.2;0.2;0.2};
+        plotInfo.fillVal = {'#0072BD';	'#D95319';'#7E2F8E'};% {[0.8 0.8 0.8],0.5,'none'};
+        plotInfo.edgeVec = {':';':';':'};% {[0.8 0.8 0.8],0.5,'none'};
+        plotInfo.fillProp_entries = [plotInfo.fillVal,faceAlpha,plotInfo.fillVal,plotInfo.edgeVec];
         t = data.kinematics.angularData.time;
         GaitInfo = getPartOfGaitData(1,data.kinematics.GaitPhaseData,t,data.kinematics.stepTimes);
-        
+        GaitInfo.tp = (0:0.5:100)';
         saveInfo.b_saveFigure = 0;
-        saveInfo.info = 'prosthetic_3D';
+        saveInfo.info = data.modelType;
         
         %%
-        %         for i = 1:numOfData
-        %             %     fieldnameSelect = structnames(contains(structnames,updateFigure.Children(i).Tag));
-        %             %     set(updateFigure, 'currentaxes', updateFigure.Children(i));
-        %             if ~isempty(updateFigure.Children) && length(findall(updateFigure,'type','axes')) == numOfData
-        %                 axesChildren = findall(updateFigure,'type','axes');
-        %                 updateProgressPlot(axesChildren(i),data.(axesChildren(i).Tag),[],b_minCost)
-        %             else
-        %                 ax = subplot(ceil(numOfData/4),4,i,axes(updateFigure)); % Select correct axis
-        %                 updateProgressPlot(ax,data.(dataFieldnames{i}),dataFieldnames{i},b_minCost) % update axis with new data
-        %             end
-        %
-        %         end
+%         for i = 1:numOfData
+%             %     fieldnameSelect = structnames(contains(structnames,updateFigure.Children(i).Tag));
+%             %     set(updateFigure, 'currentaxes', updateFigure.Children(i));
+%             if ~isempty(updateFigure.Children) && length(findall(updateFigure,'type','axes')) == numOfData
+%                 axesChildren = findall(updateFigure,'type','axes');
+%                 updateProgressPlot(axesChildren(i),data.(axesChildren(i).Tag),[],b_minCost)
+%             else
+%                 ax = subplot(ceil(numOfData/4),4,i,axes(updateFigure)); % Select correct axis
+%                 updateProgressPlot(ax,data.(dataFieldnames{i}),dataFieldnames{i},b_minCost) % update axis with new data
+%             end
+%             
+%         end
         %         drawnow;
         if b_minCost
-            
+
             warning('off');
-            
+               
             clf(gaitKinematics);
             sgtitle(gaitKinematics,['Gait kinematics for cost of ',num2str(round(data.cost.data,1)),', with $v_{mean}$ = ', num2str(round(data.vMean.data,1)),'m/s','. $\tau_{stop}$ = ', num2str(data.sumTstop.data)]);
             plotAngularData(data.kinematics.angularData,data.kinematics.GaitPhaseData,plotInfo,GaitInfo,saveInfo,gaitKinematics);
-            
+                           
             clf(musclesStimulation);
             sgtitle(musclesStimulation,['Muscle stimulation levels for cost of ',num2str(round(data.cost.data,1)),', with $v_{mean}$ = ', num2str(round(data.vMean.data,1)),'m/s','. $\tau_{stop}$ = ', num2str(data.sumTstop.data)]);
             plotMusculoData(data.kinematics.musculoData,plotInfo,GaitInfo,saveInfo,musclesStimulation);
-            
+                            
             clf(GRFData);
             sgtitle(GRFData,['Ground reaction forces for cost of ',num2str(round(data.cost.data,1)),', with $v_{mean}$ = ', num2str(round(data.vMean.data,1)),'m/s','. $\tau_{stop}$ = ', num2str(data.sumTstop.data)]);
             plotGRF(data.kinematics.GRFData,plotInfo,GaitInfo,saveInfo,GRFData);
