fbe26207b52a7dc5c67cf2b654df5d54badb049f
nathantim <timmerhout@gmail.com>
2020-09-10 10:05:28 +0200

    General setInit for healthy and amputee

--- setInitHealthy.m
+++ setInitHealthy.m
@@ -1,7 +1,7 @@
 %% set initial conditions
 % conditions from Song
 
-vx0             = paramIC(1)  	*1.3; %[m/s] 
+vx0             = paramIC(1)  	*(inner_opt_settings.target_velocity); %[m/s] 
 LphiAnkle0      = paramIC(2)   	*100*pi/180 - pi/2; %[rad]
 LphiKnee0       = pi - paramIC(3)   	*180*pi/180; %[rad]
 LphiHip0        = paramIC(4) 	*165*pi/180 - pi; %[rad]
@@ -11,13 +11,14 @@ RphiHip0        = paramIC(7)   	*200*pi/180 - pi; %[rad]
 LphiHipR0       = paramIC(8)*(-1)*pi/180; %[rad]
 RphiHipR0       = paramIC(9)*(1)*pi/180; %[rad]
 
-vy0 = paramIC(10)*.2;
+vyInit = paramIC(10)*.2*(inner_opt_settings.target_velocity/1.3);
+vzInit = 0;
 
-x0      = .2;
-y0      = hatLeftHipToCenterDistWidth;
-z0      = paramIC(11)      *.01;
-yaw0    = pi/180;
-roll0   = (-1)*pi/180;
+xInit      = .2;
+yInit      = hatLeftHipToCenterDistWidth;
+zInit      = paramIC(11)      *.01;
+yawInit    = pi/180;
+rollInit   = (-1)*pi/180;
 
 initialTargetAngle  = 70*pi/180;
 initialTargetAngleR = 90*pi/180;
