77aa851116a7642062de9a2cec048d9dff9a1ed1
sup-gjtimmers <sup-gjtimmers@tudelft.net>
2020-09-29 14:33:42 +0200

    Working 3D animation

--- Animation3D/rotTransObj.m
+++ Animation3D/rotTransObj.m
@@ -3,47 +3,47 @@
 % ---------------------------
 
 function rotTransObj( Object, LowXYZ, TopXYZ, LowXYZold, TopXYZold )
-    
-    % calculate change in rotation angle compared to previous angle
-    % Rot y
-    dbeta =  atan2(   LowXYZ(1)-   TopXYZ(1),    TopXYZ(3)-   LowXYZ(3)) ...
-           -atan2(LowXYZold(1)-TopXYZold(1), TopXYZold(3)-LowXYZold(3));
-    RotY = [cos(dbeta) 0 sin(dbeta); ...
-            0 1 0; ...
-            -sin(dbeta) 0 cos(dbeta)];
-       
-       % Rot z
-    dalpha =  atan2(   LowXYZ(1)-   TopXYZ(1),    TopXYZ(2)-   LowXYZ(2)) ...
-           -atan2(LowXYZold(1)-TopXYZold(1), TopXYZold(2)-LowXYZold(2));
-    RotZ = [cos(dalpha) -sin(dalpha) 0; ...
-            sin(dalpha) cos(dalpha) 0; ...
-            0 0 1];
-       
-    % Rot x   
-    dgamma =  atan2(   LowXYZ(2)-   TopXYZ(2),    TopXYZ(3)-   LowXYZ(3)) ...
-           -atan2(LowXYZold(2)-TopXYZold(2), TopXYZold(3)-LowXYZold(3));
-    RotX = [1 0 0; ...
-            0 cos(dgamma) -sin(dgamma); ...
-            0 sin(dgamma) cos(dgamma)];
+    if size(LowXYZ,1) == 1
+        LowXYZ = LowXYZ';
+    end
+    if size(TopXYZ,1) == 1
+        TopXYZ = TopXYZ';
+    end
+    if size(LowXYZold,1) == 1
+        LowXYZold = LowXYZold';
+    end
+    if size(TopXYZold,1) == 1
+        TopXYZold = TopXYZold';
+    end
         
     % get actual x and z data and shift it back to zero
     xAct = get(Object, 'XData')-LowXYZold(1);
-    yAct = get(Object, 'YData');%-LowXYZold(2);
+    yAct = get(Object, 'YData')-LowXYZold(2);
     zAct = get(Object, 'ZData')-LowXYZold(3);
     
-    RotMat = RotX * RotY * RotZ;
+
     
-%     newData = RotMat * [xAct;yAct;zAct] + LowXYZ;
-    % rotate and shift x and z data to new angle and position
-    xNew = cos(dbeta)*xAct - sin(dbeta)*zAct + LowXYZ(1);
-    yNew = yAct;% + LowXYZ(2);
-    zNew = sin(dbeta)*xAct + cos(dbeta)*zAct + LowXYZ(3);
+    magOld = sqrt(sum((TopXYZold-LowXYZold).^2));
+    magNew = sqrt(sum((TopXYZ-LowXYZ).^2));
+    if abs(magOld-magNew) > 1E-2
+        warning('Dissimilar magnitudes, error of: %d',abs(magOld-magNew));
+    end
+    oldUnit = ((TopXYZold-LowXYZold)./magOld);
+    newUnit = ((TopXYZ-LowXYZ)./magNew);
+
+    x = oldUnit;
+    y = newUnit;
+    RotMat = eye(3) + y*x' - x*y' + 1/(1+dot(x,y))*(y*x' - x*y')^2;
     
-%     xNew = RotMat(1,1)*xAct + RotMat(1,2)*yAct + RotMat(1,3)*zAct;
-%     yNew = RotMat(2,1)*xAct + RotMat(2,2)*yAct + RotMat(2,3)*zAct;
-%     zNew = RotMat(3,1)*xAct + RotMat(3,2)*yAct + RotMat(3,3)*zAct;
-%     
+    xNew = RotMat(1,1)*xAct + RotMat(1,2)*yAct + RotMat(1,3)*zAct + LowXYZ(1);
+    yNew = RotMat(2,1)*xAct + RotMat(2,2)*yAct + RotMat(2,3)*zAct + LowXYZ(2);
+    zNew = RotMat(3,1)*xAct + RotMat(3,2)*yAct + RotMat(3,3)*zAct + LowXYZ(3);
+
+%     newVec =  R*[xAct;yAct;zAct] + LowXYZ;
     % update cone object
+    if max(abs(RotMat*(TopXYZold-LowXYZold) + (LowXYZ) - (TopXYZ))) > 1E-2
+        warning('Wrong rotation matrix, max error of: %d',max(abs(RotMat*(TopXYZold-LowXYZold) + (LowXYZ) - (TopXYZ))));
+    end
     set(Object, 'XData', xNew, 'yData', yNew, 'ZData', zNew);
 
 end
