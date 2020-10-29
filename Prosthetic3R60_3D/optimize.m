ee5d4ca72be7ea6030f7d82a6101e8e24b179bca
sup-gjtimmers <sup-gjtimmers@tudelft.net>
2020-10-29 17:38:54 +0100

    Fixing getCost, initialize dataStruct

--- Prosthetic3R60_3D/optimize.m
+++ Prosthetic3R60_3D/optimize.m
@@ -11,14 +11,14 @@ clear all; close all; clc;
 
 %%
 b_resumeOptimization = char(input("Do you want to resume a previous optimization? (yes/no)   ",'s'));
-optimizationInfo = 'numerical_change';
+optimizationInfo = 'updated_model';
 
 %%
 % initial_gains_filename = ['Results' filesep 'Rough' filesep 'Umb10_1.5cm_1.2ms_kneelim1_mstoptorque2.mat'];
 % initial_gains_filename = ['Results' filesep 'Rough' filesep 'Umb10_1.5cm_0.9ms_opt_1.2mscoronal.mat'];
 % initial_gains_filename = ['Results' filesep 'Rough' filesep 'Umb10_1.2ms_difffoot_higherabd.mat'];
 % initial_gains_filename = ['Results' filesep 'Rough' filesep 'Umb10_0.9ms.mat'];
-initial_gains_filename = ['Results' filesep 'Rough' filesep 'Umb10_1.2ms_wheading.mat'];
+initial_gains_filename = ['Results' filesep 'Rough' filesep 'Umb10_0.9ms_num_inter.mat'];
 
 
 %%
@@ -60,9 +60,9 @@ animFrameRate = 30;
 % save_system(model)
 
 %% Build the Rapid Accelerator target once
-warning('off')
+
 rtp = Simulink.BlockDiagram.buildRapidAcceleratorTarget(model);
-warning('on')
+
 
 %% setup cmaes
 numvars = length(InitialGuess);
