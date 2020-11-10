e78619a2fa378d024c2aa8a3417b48e054fc9cfe
nathantim <timmerhout@gmail.com>
2020-11-10 16:00:52 +0100

    update

---
+++ Prosthetic3R60CMG_3D/Results/2020-10-21_19-23_1.2ms_with_heading_HATPos_x/OptimParamsCapture.m
@@ -0,0 +1,8 @@
+% literature velocity: [0.5,0.9,1.2]
+target_velocity = 1.2;
+min_velocity = target_velocity;
+% min_velocity = 0.5;
+max_velocity = target_velocity; 
+% max_velocity = 299792458.0; % speed of light
+initiation_steps = 5;
+dist_slack = 0.4;
