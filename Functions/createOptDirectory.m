b8a231184cf8cb767164c2510c7e58a4344c4a97
nathantim <timmerhout@gmail.com>
2020-11-16 12:12:59 +0100

    Implement input parser in setInnerOptSettings

--- Functions/createOptDirectory.m
+++ Functions/createOptDirectory.m
@@ -1,10 +1,16 @@
-function [inner_opt_settings, opts] = createOptDirectory(modelDir,inner_opt_settings,opts,optimizationInfo)
-% Function creates a directory in the Results folder located in the directory
-% of the NeuroMuscular model file. This directory contains the vcmaes file
-% which contains saved intermediate steps of the optimization, and it
-% contains successfull runs (meaning full simulation time was reached)
-% If resume is true, than it selects the latests directory containing the
-% same optimization info and target velocity
+function [setInnerOptSettings, opts] = createOptDirectory(modelDir,setInnerOptSettings,opts,optimizationInfo)
+%CREATEOPTDIRECTORY             Function creates a directory in the Results folder located in the directory
+%                               of the NeuroMuscular model file. This directory contains the vcmaes file
+%                               which contains saved intermediate steps of the optimization, and it
+%                               contains successfull runs (meaning full simulation time was reached)
+%                               If resume is true, than it selects the latests directory containing the
+%                               same optimization info and target velocity
+% INPUTS:
+%   - modelDir                  Directory of the model that is being optimized
+%   - setInnerOptSettings       Already created settings, to which the optimization directory is added
+%   - opts                      Already created options, to which the optimization directory is set as base directory
+%   - optimizationInfo          Extra info added to the optimization directory name
+%%
 baseDirParts = strsplit(modelDir,filesep);
 baseDir = baseDirParts{1};
 for idx = 2:length(baseDirParts)-1
@@ -14,43 +20,35 @@ end
 if contains(num2str(opts.Resume),'no') || min(opts.Resume) == 0
     dateNow = strcat(char(datestr(now,'yyyy-mm-dd_HH-MM')),'_');
     
-    folderName = [dateNow, num2str(inner_opt_settings.target_velocity) 'ms_' optimizationInfo];
+    folderName = [dateNow, num2str(setInnerOptSettings.target_velocity) 'ms_' optimizationInfo];
     optDirectory = [modelDir, filesep, 'Results', filesep, folderName];
     mkdir(optDirectory);
-    inner_opt_settings.optimizationDir = optDirectory;
+    setInnerOptSettings.optimizationDir = optDirectory;
     
-    init_gains = strsplit(string(opts.UserData),' ');
-    init_gains = init_gains{end};
-    init_gains_name = strsplit(string(opts.UserData),filesep);
-    init_gains_name = init_gains_name{end};
-    opts.UserData = ['Gains filename: ' optDirectory, filesep init_gains_name];
-    moveAndRenameFile([modelDir filesep init_gains], [optDirectory, filesep 'initial_gains.mat']);
+    moveAndRenameFile([modelDir filesep innerOptSettings.initialGainsFilename], [optDirectory, filesep 'initialGains.mat']);
+    if ~isempty(init_gainsCMG)
+        moveAndRenameFile([modelDir filesep innerOptSettings.initialCMGGainsFilename], [optDirectory, filesep 'initialGainsCMG.mat']);
+        moveAndRenameFile([baseDir filesep 'CMGParams.m'], [optDirectory, filesep,'CMGParamsCapture.m']);
+    end
+    
     moveAndRenameFile([baseDir filesep 'BodyMechParams.m'], [optDirectory, filesep,'BodyMechParamsCapture.m']);
     moveAndRenameFile([modelDir filesep 'ControlParams.m'], [optDirectory, filesep,'ControlParamsCapture.m']);
     moveAndRenameFile([baseDir filesep 'Prosthesis3R60Params.m'], [optDirectory, filesep,'Prosthesis3R60ParamsCapture.m']);
-    moveAndRenameFile([baseDir filesep 'OptimParams.m'], [optDirectory, filesep,'OptimParamsCapture.m']);
     
-%     opts.SaveFilename = [optDirectory filesep 'vcmaes.mat'];
     opts.BaseDirectory = optDirectory;
-    save([optDirectory filesep 'settings.mat'],'inner_opt_settings','opts');  
+    save([optDirectory filesep 'settings.mat'],'setInnerOptSettings','opts');  
     
     
 elseif contains(num2str(opts.Resume),'yes') || min(opts.Resume) == 1
-%     folderNameContains = [num2str(inner_opt_settings.target_velocity) 'ms_' optimizationInfo];
-%     
-%     existFolders = dir([modelDir, filesep, 'Results']);
-%     existFoldersNames = {existFolders.name};
-%     
-%     folderName = existFoldersNames{ find((contains(existFoldersNames,folderNameContains)) == 1,1,'last') };
-    optDirectory = uigetdir([modelDir, filesep, 'Results']); %[modelDir, filesep, 'Results', filesep, folderName];
+    optDirectory = uigetdir([modelDir, filesep, 'Results']);
     
     prevSettings = load([optDirectory filesep 'settings.mat']);
     opts = prevSettings.opts;
     opts.Resume = 'yes';
-    inner_opt_settings = prevSettings.inner_opt_settings;
+    setInnerOptSettings = prevSettings.setInnerOptSettings;
     
 elseif contains(num2str(opts.Resume),'eval')
-    inner_opt_settings.optimizationDir = ' ';
+    setInnerOptSettings.optimizationDir = ' ';
 else
     error('Unknown resume value');
 end
--- Functions/setInnerOptSettings.m
+++ Functions/setInnerOptSettings.m
@@ -1,34 +1,40 @@
 function [innerOptSettings, opts] = setInnerOptSettings(model,varargin)
-% PLOTDATA                          Function that plots the data simulation
+% SETINNEROPTSETTINGS               Function that plots the data simulation
 %
 % INPUTS:
+%   - model                         Name of model that is being evaluated or optimized.
 %   - varargin                      Variable inputs can be given, which will result in affecting the plot, or adding plots etc
 %                                   Use: 
-%                                   plotData(GaitPhaseData,stepTimes,'nameVarArgin1',<value/data varargin1> ,'nameVarArgin2',<value/data varargin2>)
-%                                   Required varargin:
-%                                   - 'GaitPhaseData': structure with the gait phase data from the simulation 
-%                                   - 'stepTimes': structure with the step time data from simulation.
+%                                   setInnerOptSettings(model,'nameVarArgin1',<value/data varargin1> ,'nameVarArgin2',<value/data varargin2>)
 %                                   Optional varargin:
-%                                   - 'angularData': structure with time with angular data from simulation  
-%                                   - 'musculoData': structure with time with muscular data from simulation 
-%                                   - 'GRFData': structure with time with GRF data from simulation 
-%                                   - 'jointTorquesData': structure with time with joint torque data from simulation 
-%                                   - 'CMGData': structure with time with CMG data from simulation 
-%                                   - 'saveFigure': bool for saving figure, default: false
-%                                   - 'showAverageStride': bool for showing data averaged per stride, default: true
-%                                   - 'showSD': bool for showing std data per stride, default: true
-%                                   - 'showFukuchi': bool for showing Fukuchi data, default: false
-%                                   - 'info': char with information that can be added to figure saved file name
-%                                   - 'timeInterval': time interval over which to show the data
-% 
-%   - amputeeCMGNotActiveData       Optional, structure with the data from amputee gait with inactive CMG simulation.
-%   - amputeeCMGActiveData          Optional, Structure with the data from amputee gait with active CMG simulation.
-%   - info                          Optional, info which can be added to the saved file name of the figure
-%   - b_saveTotalFig                Optional, select whether to save the figure or not, default is false
+%                                   - 'initialGainsFilename': .mat file with the initial neurological controller gains  
+%                                   - 'initialCMGGainsFilename': s.mat file with the initial CMG controller gains  
+%                                   - 'expenditureModel': Expenditure model which should be used in the cost function.                      Default: 'Umberger (2010)'. 
+%                                   - 'resume': Select whether to resume a previous optimization, or just do evaluation (yes/no/eval).      Default: 'no'.
+%                                   - 'optimizationInfo': Information which is to be added in the optimization directory name.
+%                                   - 'timeFactor': Factor with which to multiply the time cost.                                            Default: 100000. 
+%                                   - 'velocityFactor': Factor with which to multiply the velocity cost.                                    Default: 100.
+%                                   - 'CoTFactor': Factor with which to multiply the cost of transport cost.                                Default: 1.
+%                                   - 'sumStopTorqueFactor': Factor with which to multiply the sum of stop torque cost.                     Default: 1E-1.
+%                                   - 'CMGdeltaHFactor': Factor with which to multiply the exchanged angular momentum cost.                 Default: 15.
+%                                   - 'terrainHeight': Terrain height used during optimization.                                             Default: 0.01 m .
+%                                   - 'numTerrains': Number of terrains to evaluate during optimization (of which 1 is flat).               Default: 4.
+%                                   - 'targetVelocity': Velocity with which the model has to walk.                                          Default: 0.9 m/s.
+%                                   - 'minVelocity': Minimal velocity with which the model has to walk.                                     Default: 0.5 m/s.
+%                                   - 'maxVelocity': Maximal velocity with which the model is allowed to walk.                              Default: speed of light 
+%                                   - 'minLimbDistance': Minimal distance between center line of limbs that has to be satisfied.            Default: 0.02 m.
+%                                   - 'initiationSteps': Number of initial steps which are not included in the cost function evaluation.    Default: 5.
+%                                   - 'numParWorkers': Number of parallel workers during optimization.                                      Default: Number of cores available.
+%                                   - 'timeOut': Maxmimum computation time simulation of the model can take.                                Default: 10 minutes.
+%                                   - 'maxIter': Maximum number of iterations of the CMAES algorithm.                                       Default: 300.
+%                                   - 'createVideo': Create a video of the simulation if a new best evaluation is found.                    Default: true.
 %
 % OUTPUTS:
-%   -
+%   - innerOptSettings              - All the inner optimization settings
+%   used in GETCOST, or PLOTPROGRESSOPTIMIZATION
+%   - opts
 %%
+opts = [];
 if ~isempty(model)
     load_system(model);
 end
@@ -40,98 +46,99 @@ persistent p
 if isempty(p)
     p = inputParser;
     p.FunctionName = 'setInnerOptSettings';
-    
-
         
     validFileFcn = @(ii) exist(ii,'file') ==2;
     addParameter(p,'initialGainsFilename','',validFileFcn);
+    addParameter(p,'initialCMGGainsFilename','',validFileFcn);
     
     % Umberger (2003), Umberger (2003) TG, Umberger (2010), Wang (2012) are valid
     validExpModelFcn = @(ii) sum(contains(getExpenditureModels(model),ii))>0;
     addParameter(p,'expenditureModel','Umberger (2010)',validExpModelFcn);
     
     validResumeFcn = @(ii) contains(ii,'yes') || contains(ii,'no') || contains(ii,'eval');
-    addParameter(p,'resume','eval',validResumeFcn);
+    addParameter(p,'resume','no',validResumeFcn);
     
     validCharFcn = @(ii) ischar(ii);
     addParameter(p,'optimizationInfo','',validCharFcn);
     
     validValueFcn = @(ii) isreal(ii);
+    % Cost function factors
     addParameter(p,'timeFactor',            100000,             validValueFcn);
     addParameter(p,'velocityFactor',        100,                validValueFcn);
     addParameter(p,'CoTFactor',             1,                  validValueFcn);
     addParameter(p,'sumStopTorqueFactor',   1E-2,               validValueFcn);
-    addParameter(p,'CMGTorqueFactor',       0,                  validValueFcn);
-    addParameter(p,'CMGdeltaHFactor',       15,                 validValueFcn);
-    addParameter(p,'ControlRMSEFactor',     0,                  validValueFcn);
-    addParameter(p,'selfCollisionFactor',   1000,               validValueFcn);
-    addParameter(p,'terrain_height',        0.010,              validValueFcn);
+    addParameter(p,'CMGdeltaHFactor',       15,                 validValueFcn); 
+    
+    addParameter(p,'terrainHeight',         0.010,              validValueFcn); % m
+    addParameter(p,'targetVelocity',        0.9,                validValueFcn); % m/s
+    addParameter(p,'minVelocity',           0.5,                validValueFcn); % m/s
+    addParameter(p,'maxVelocity',           299792458,          validValueFcn); % m/s, speed of light
+    
+    addParameter(p,'minLimbDistance',       0.02,               validValueFcn); % m, minimal distance betweeen limbs
     
     validIntegerFcn = @(ii) isinteger(ii);
-    addParameter(p,'numTerrains',       4,  validIntegerFcn);
-    addParameter(p,'initiationSteps',   5,  validIntegerFcn);
+    addParameter(p,'numTerrains',       4,                  validIntegerFcn);
+    addParameter(p,'initiationSteps',   5,                  validIntegerFcn);
+    addParameter(p,'numParWorkers',     maxNumCompThreads,  validIntegerFcn);
+    addParameter(p,'timeOut',           10*60,              validIntegerFcn); % Time out for simulation
+    addParameter(p,'maxIter',           300,              validIntegerFcn);
+    
+    validBoolFcn = @(ii) isscalar(ii) && islogical(ii);
+    addParameter(p,'createVideo',     true,  validBoolFcn);  % Create video during optimization
     
 end
 
 parse(p,varargin{:});
-opts.Resume             = p.Results.resume;
-initialGainsFilename    = p.Results.initialGainsFilename;
 
 %%
+opts.Resume                                 = p.Results.resume;
+innerOptSettings.initialGainsFilename       = p.Results.initialGainsFilename;
+innerOptSettings.initialCMGGainsFilename    = p.Results.initialCMGGainsFilename;
 
-%%
-OptimParams;
-opts = [];
+innerOptSettings.expenditure_model      = p.Results.expenditureModel;   %'Umberger (2010)';
+innerOptSettings.numTerrains            = p.Results.numTerrains;
+innerOptSettings.terrainHeight          = p.Results.terrainHeight; % in m
 
-
-innerOptSettings.expenditure_model      = p.Results.expenditureModel;%'Umberger (2010)';
+% Cost function factors
 innerOptSettings.timeFactor             = p.Results.timeFactor;
 innerOptSettings.velocityFactor         = p.Results.velocityFactor;
-innerOptSettings.CoTFactor              = p.Results.CoTFactor; % cost of transport
+innerOptSettings.CoTFactor              = p.Results.CoTFactor;      % cost of transport
 innerOptSettings.sumStopTorqueFactor    = p.Results.sumStopTorqueFactor;
-innerOptSettings.CMGTorqueFactor        = p.Results.CMGTorqueFactor;
 innerOptSettings.CMGdeltaHFactor        = p.Results.CMGdeltaHFactor;
-innerOptSettings.ControlRMSEFactor      = p.Results.ControlRMSEFactor;
-innerOptSettings.selfCollisionFactor    = p.Results.selfCollisionFactor;
 
-innerOptSettings.numTerrains = 4;
-innerOptSettings.terrain_height = 0.010; % in m
-if usejava('desktop')
-    innerOptSettings.numParWorkers = 4;
-    innerOptSettings.visual = true;
+if ~usejava('desktop') || batchStartupOptionUsed
+    innerOptSettings.visual             = false;
 else
-    innerOptSettings.numParWorkers = 4;
-    innerOptSettings.visual = false;
+    innerOptSettings.visual             = true;
 end
-innerOptSettings.initiation_steps = initiation_steps;
-innerOptSettings.target_velocity = target_velocity;
-innerOptSettings.min_velocity = min_velocity;
-innerOptSettings.max_velocity = max_velocity;
 
-innerOptSettings.timeOut = 10*60; % Time out for simulation
-innerOptSettings.createVideo = true; % Create video during optimization
-if nargin > 0
-    opts = cmaes;
-    %opts.PopSize = numvars;
-    
-    opts.MaxIter = 300;
-    % opts.StopFitness = -inf;
-    opts.StopFitness = 0;
-    opts.DispModulo = 1;
-    opts.TolX = 1e-2;
-    opts.TolFun = 1e-2;
-    opts.EvalParallel = 'yes';
-    opts.LogPlot = 'off';
-    if (min_velocity == target_velocity && max_velocity == target_velocity)
-        opts.TargetVel = target_velocity;
-    end
-    opts.UserData = char(strcat("Gains filename: ", initialGainsFilename));
-    opts.UserDat2 = '';
-    fields_opts = fields(innerOptSettings);
-    for ii = 1:length(fields(innerOptSettings))
-        opts.UserDat2 = strcat(opts.UserDat2,"; ", fields_opts{ii}, ": ", string(innerOptSettings.(fields_opts{ii})));
-    end
-    
+innerOptSettings.numParWorkers          = p.Results.numParWorkers;
+
+innerOptSettings.targetVelocity         = p.Results.targetVelocity; % m/s
+if ~isempty(innerOptSettings.targetVelocity)
+    innerOptSettings.minVelocity        = innerOptSettings.targetVelocity;
+    innerOptSettings.maxVelocity        = innerOptSettings.targetVelocity;
+else
+    innerOptSettings.minVelocity        = p.Results.minVelocity;
+    innerOptSettings.maxVelocity        = p.Results.maxVelocity; 
+end
+
+innerOptSettings.initiationSteps        = p.Results.initiationSteps;
+innerOptSettings.minLimbDistance        = p.Results.minLimbDistance;
+
+innerOptSettings.timeOut                = p.Results.timeOut; 
+innerOptSettings.createVideo            = p.Results.createVideo;
+
+if ~isempty(initialGainsFilename)
+    opts                                = cmaes;    
+    opts.MaxIter                        = p.Results.maxIter;
+    opts.StopFitness                    = 0;
+    opts.DispModulo                     = 1;
+    opts.TolX                           = 1e-2;
+    opts.TolFun                         = 1e-2;
+    opts.EvalParallel                   = 'yes';
+    opts.LogPlot                        = 'off';
+   
     [innerOptSettings, opts] = createOptDirectory(pwd,innerOptSettings,opts,optimizationInfo);
     
 end
