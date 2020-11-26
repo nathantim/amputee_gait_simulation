6881ef8f65cafe4491356e5f88b646608ade83cd
nathantim <timmerhout@gmail.com>
2020-11-26 13:35:40 +0100

    Zoom out a bit

--- Animation/viewProsthesis.m
+++ Animation/viewProsthesis.m
@@ -6,7 +6,7 @@ function viewProsthesis(figAxes, u, ViewWin)
     
     betweenThighShank = mean([u(22:24);u(59:61)],1); % Mean between right thigh and shank connect
     % shift to align with hip
-    zoomfactor = 16;
+    zoomfactor = 10;
     set(figAxes, 'XLim', [betweenThighShank(1) - ViewWin/zoomfactor,  betweenThighShank(1) + ViewWin/zoomfactor]);
     set(figAxes, 'YLim', [betweenThighShank(2) - ViewWin/zoomfactor,  betweenThighShank(2) + ViewWin/zoomfactor]);
     set(figAxes, 'ZLim', [betweenThighShank(3) - ViewWin/zoomfactor,  betweenThighShank(3) + ViewWin/zoomfactor]);
